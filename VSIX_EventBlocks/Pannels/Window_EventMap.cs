6870eb804d8e98fb9d06d33f8e042ca00216cb5b
Florent <florent.barthelemy@protonmail.com>
2020-05-15 15:34:10 +0200

    First commit

---
+++ VSIX_EventBlocks/Pannels/Window_EventMap.cs
@@ -0,0 +1,34 @@
+﻿namespace VSIX_EventBlocks
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("edb31a4e-511f-41ed-b807-43d5f2a35656")]
+    public class Window_EventMap : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Window_EventMap"/> class.
+        /// </summary>
+        public Window_EventMap() : base(null)
+        {
+            this.Caption = "Window_EventMap";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new Window_EventMapControl();
+        }
+    }
+}
---
+++ VSIX_EventBlocks/Pannels/Window_EventMapControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace VSIX_EventBlocks
+{
+    using System.ComponentModel.Design;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Web.UI.Design.WebControls;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Forms;
+    using System.Windows.Forms.Design;
+    using Microsoft.VisualStudio.Designer;
+
+    public partial class Window_EventMapControl : UserControl
+    {
+
+        public Window_EventMapControl()
+        {
+            this.InitializeComponent();
+        }
+        
+
+    }
+}
